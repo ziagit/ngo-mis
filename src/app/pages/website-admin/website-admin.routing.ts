678cd870f07abd44c907b6d63c27495dcc6c4a65
zia <zia.csco@gmail.com>
2019-11-01 13:01:43 -0700

    worked on website-admin module

--- src/app/pages/website-admin/website-admin.routing.ts
+++ src/app/pages/website-admin/website-admin.routing.ts
@@ -1,9 +1,12 @@
 import { WebsiteAdminComponent } from './website-admin.component';
 import { AboutUsComponent } from './components/about-us/about-us.component';
-import { BiographyComponent } from './components/biography/biography.component';
 import { Routes, RouterModule } from "@angular/router";
 import { ContactUsComponent } from './components/contact-us/contact-us.component';
-import { SlideshowsComponent } from './components/slideshows/slideshows.component';
+import { HomeComponent } from './components/home/home.component';
+import { NewsComponent } from './components/news/news.component';
+import { OrganizationLawsComponent } from './components/organization-laws/organization-laws.component';
+import { JobsComponent } from './components/jobs/jobs.component';
+import { OrgRegisterationComponent } from './components/org-registeration/org-registeration.component';
 
 
 const routes: Routes = [
@@ -11,12 +14,14 @@ const routes: Routes = [
         path: '',
         component: WebsiteAdminComponent,
         children: [
-            { path: '', redirectTo: 'web', pathMatch: 'full' },
-            { path: 'biography', component: BiographyComponent },
+            { path: '', redirectTo: 'home', pathMatch: 'full' },
+            { path: 'home', component: HomeComponent },
             { path: 'about-us', component: AboutUsComponent },
+            { path: 'org-registeration', component: OrgRegisterationComponent },
+            { path: 'news', component: NewsComponent },
+            { path: 'org-laws', component: OrganizationLawsComponent },
+            { path: 'jobs', component: JobsComponent },
             { path: 'contact-us', component: ContactUsComponent },
-            { path: 'slideshows', component: SlideshowsComponent }
-
         ]
     }
 ]
