7020c5475321b30882c3c12b6a9c70b4c8a8adca
zia <zia.csco@gmail.com>
2019-11-01 06:24:12 -0700

    worked on material design

--- src/app/pages/website-admin/website-admin.module.ts
+++ src/app/pages/website-admin/website-admin.module.ts
@@ -5,12 +5,14 @@ import { CommonModule } from '@angular/common';
 import { WebsiteAdminComponent } from './website-admin.component';
 import { BiographyComponent } from './components/biography/biography.component';
 import { AboutUsComponent } from './components/about-us/about-us.component';
+import { SlideshowsComponent } from './components/slideshows/slideshows.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
     WebsiteAdminComponent, 
     BiographyComponent, 
-    AboutUsComponent
+    AboutUsComponent, SlideshowsComponent, ContactUsComponent
   ],
   imports: [
     CommonModule,
--- src/app/pages/website-admin/website-admin.routing.ts
+++ src/app/pages/website-admin/website-admin.routing.ts
@@ -2,16 +2,20 @@ import { WebsiteAdminComponent } from './website-admin.component';
 import { AboutUsComponent } from './components/about-us/about-us.component';
 import { BiographyComponent } from './components/biography/biography.component';
 import { Routes, RouterModule } from "@angular/router";
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { SlideshowsComponent } from './components/slideshows/slideshows.component';
 
 
-const routes: Routes =[
+const routes: Routes = [
     {
         path: '',
         component: WebsiteAdminComponent,
-        children:[
-            {path: '', redirectTo: 'web', pathMatch: 'full'},
-            {path: 'biography', component: BiographyComponent},
-            {path: 'about-us', component: AboutUsComponent}
+        children: [
+            { path: '', redirectTo: 'web', pathMatch: 'full' },
+            { path: 'biography', component: BiographyComponent },
+            { path: 'about-us', component: AboutUsComponent },
+            { path: 'contact-us', component: ContactUsComponent },
+            { path: 'slideshows', component: SlideshowsComponent }
 
         ]
     }
