c3c6058eabca8a4a4e8a37bea67313e07da2596f
DoD Admin <DoD Admin>
2019-11-02 17:13:11 +0430

    role created

--- server/app/Mis/User.php
+++ server/app/Mis/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\MIS;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
@@ -36,4 +36,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function role()
+    {
+        return $this->belongsToMany(Role::class,"role_id","id");
+    }
 }
