12cd113f34b700adcea6137ac2b5cfebe08d2411
arifrahimi <arifhussain.kpu@gmail.com>
2019-11-03 05:20:49 +0430

    Orginaztion

--- src/app/pages/organization/components/org-list/org-list.service.ts
+++ src/app/pages/organization/components/org-list/org-list.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import {IOrganization} from './Organization';
 import { Observable } from 'rxjs';
 
@@ -7,10 +7,19 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class OrgListService {
-  	url: string='http://localhost:8000/api/';
-  constructor(private http: HttpClient) { }
+    url: string='http://localhost:8000/api/';
+    headers:HttpHeaders = new HttpHeaders();
+  options:any;
+
+  constructor(private http: HttpClient) { 
+    this.headers.append("enctype","mutipart/form-data");
+    this.headers.append("Component-Type","application/json");
+    this.headers.append("X-Requested-With","XMLHttpRequest");
+  }
 
   getOrg():Observable<IOrganization[]>{
-    return this.http.get<IOrganization[]>(this.url+'getorganization');
+    return this.http.get<IOrganization[]>(this.url+'organizations');
+    
   }
+  
 }
