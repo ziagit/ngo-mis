4700f810ac6d450b89bace36e2ff4b7812c6024c
zia <zia.csco@gmail.com>
2019-11-03 22:17:44 -0800

    assessment component deleted

--- src/app/pages/settings/settings.module.ts
+++ src/app/pages/settings/settings.module.ts
@@ -10,7 +10,6 @@ import { DistrictsComponent } from './components/loopups/districts/districts.com
 import { LookupsComponent } from './components/loopups/lookups.component';
 import { TypesComponent } from './components/types/types.component';
 import { BudjetTypesComponent } from './components/types/budjet-types/budjet-types.component';
-import { AssessmentTypesComponent } from './components/types/assessment-types/assessment-types.component';
 import { DelegationTypesComponent } from './components/types/delegation-types/delegation-types.component';
 import { EmployeeTypesComponent } from './components/types/employee-types/employee-types.component';
 import { EquipmentTypesComponent } from './components/types/equipment-types/equipment-types.component';
@@ -43,7 +42,6 @@ import { RegisterComponent } from './components/users-management/register/regist
     LookupsComponent,
     TypesComponent,
     BudjetTypesComponent,
-    AssessmentTypesComponent,
     DelegationTypesComponent,
     EmployeeTypesComponent,
     EquipmentTypesComponent,
